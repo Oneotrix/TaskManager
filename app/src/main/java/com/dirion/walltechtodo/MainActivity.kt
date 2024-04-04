6290115d205eb17d5d0da122af8a77a2a290e51c
Oneotrix <m5555hutov@gmail.com>
2024-04-04 15:55:13 +0300

    update tab navigation

--- app/src/main/java/com/dirion/walltechtodo/MainActivity.kt
+++ app/src/main/java/com/dirion/walltechtodo/MainActivity.kt
@@ -2,14 +2,21 @@ package com.dirion.walltechtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dirion.walltechtodo.databinding.ActivityMainBinding
 import com.dirion.walltechtodo.di.presentation.subcomponents.ActivityComponent
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setTabListener()
         activityComponent = App.presentationComponent
             .activityComponentBuilder()
             .activity(this@MainActivity)
@@ -17,7 +24,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun setTabListener() {
+        binding.vgTabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab) {
+                when(p0.position) {
+                    0 -> {
+                        activityComponent.navigationController().navigate(R.id.action_settingsFragment_to_tasksFragment)
+                    }
 
+                    1 -> {
+                        activityComponent.navigationController().navigate(R.id.action_tasksFragment_to_settingsFragment)
+                    }
+                }
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+            }
+
+        })
+    }
 
     companion object {
 
